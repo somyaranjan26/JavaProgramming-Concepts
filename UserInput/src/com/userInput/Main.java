8a7c894751861ba9b183d9d13dcaf7b31b27ee16
Somyaranjan Rout <45004977+somyaranjan26@users.noreply.github.com>
2019-08-18 17:38:39 +0530

    Add files via upload

---
+++ UserInput/src/com/userInput/Main.java
@@ -0,0 +1,33 @@
+package com.userInput;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // creating instance of class Scanner
+
+        System.out.print("Enter Your year of birth: ");
+
+        boolean isInt = input.hasNextInt();
+        if (isInt) {
+            int birthYear = input.nextInt();    // nextInt() store integer input in a variable
+            input.nextLine();   // handle next line character (enter key)
+
+            System.out.print("Enter Your Name: ");
+            String name = input.nextLine();
+
+            int age = 2019 - birthYear;
+
+            if (age >= 0 && age <= 100){
+                System.out.println("Welcome " + name + ", And you are " + age + " years old.");
+            } else {
+                System.out.println("You have entered INVALID BIRTH YEAR.");
+            }
+        } else {
+            System.out.println("You have entered INVALID INPUT.");
+        }
+
+        input.close();
+    }
+}
